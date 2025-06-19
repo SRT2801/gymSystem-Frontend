431170c563c8fec279fac58f75e622b76faf2b98
SRT DEV <sahirruiz4@gmail.com>
2025-06-18 19:59:50 -0500

    FEAT: Refactor header component with responsive design and mobile menu functionality

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,16 +1,56 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener } from '@angular/core';
+import { RouterModule, Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Boton } from '../boton/boton';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterModule,],
+  imports: [RouterModule, CommonModule, Boton],
   templateUrl: './header.html',
   styleUrl: './header.scss',
+  standalone: true,
 })
 export class Header {
-  isMenuOpen = false;
+  isMobileMenuOpen = false;
+  scrolled = false;
 
-  toggleMenu() {
-    this.isMenuOpen = !this.isMenuOpen;
+  constructor(private router: Router) {}
+
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    this.scrolled = window.scrollY > 50;
+  }
+
+  @HostListener('window:resize', [])
+  onResize() {
+
+    if (window.innerWidth > 992 && this.isMobileMenuOpen) {
+      this.isMobileMenuOpen = false;
+    }
+  }
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+
+   
+    if (this.isMobileMenuOpen) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+    document.body.style.overflow = '';
+  }
+
+  navigateTo(route: string) {
+    this.router.navigate([route]);
+  }
+
+  navigateToAndClose(route: string) {
+    this.closeMobileMenu();
+    this.navigateTo(route);
   }
 }

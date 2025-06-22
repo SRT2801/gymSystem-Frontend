a61d289e3ec4d87f85c44bb44a189147b3331529
SRT DEV <sahirruiz4@gmail.com>
2025-06-22 17:59:39 -0500

    FEAT: add footer as component

--- src/app/components/footer/footer.ts
+++ src/app/components/footer/footer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { ScrollAnimationDirective } from '../../directives/scroll-animation.directive';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule, ScrollAnimationDirective],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss'
+  styleUrl: './footer.scss',
+  standalone: true,
 })
-export class Footer {
-
-}
+export class Footer {}

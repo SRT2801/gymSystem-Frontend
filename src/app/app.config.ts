d95c0f1fecf53eb8e7328fe9fcd709051c396d29
SRT DEV <sahirruiz4@gmail.com>
2025-06-22 22:10:50 -0500

    FEAT: Refactor app.config.ts to enhance HTTP client configuration and add view transitions

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,16 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+  withXsrfConfiguration,
+  HTTP_INTERCEPTORS,
+} from '@angular/common/http';
+import { CredentialsInterceptor } from './interceptors/credentials.interceptor';
 
 import { routes } from './app.routes';
 
@@ -7,6 +18,29 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes)
-  ]
+    provideRouter(
+      routes,
+      withViewTransitions({
+        onViewTransitionCreated: () => {
+          // Forzar el scroll al inicio en cada navegación
+          window.scrollTo(0, 0);
+        },
+      })
+    ),
+    // Configuración para que todas las solicitudes HTTP envíen cookies
+    provideHttpClient(
+      withInterceptorsFromDi(),
+      withXsrfConfiguration({
+        // Protección CSRF (opcional pero recomendado)
+        cookieName: 'XSRF-TOKEN',
+        headerName: 'X-XSRF-TOKEN',
+      })
+    ),
+    // Registrando el interceptor para todas las solicitudes HTTP
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: CredentialsInterceptor,
+      multi: true,
+    },
+  ],
 };

2c38855e7583f6b9ca2d014d83c753bdd9261bcf
SRT DEV <sahirruiz4@gmail.com>
2025-06-23 21:22:02 -0500

    FEAT: Refactor routing to include public and authenticated layouts with respective guards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,16 +4,50 @@ import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { AdminComponent } from './pages/admin/admin';
 import { AdminGuard } from './guards/admin.guard';
+import { AuthGuard } from './guards/auth.guard';
+import { AuthenticatedLayoutComponent } from './layouts/authenticated-layout/authenticated-layout';
+import { PublicLayoutComponent } from './layouts/public-layout/public-layout';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
-  { path: 'home', component: Home },
-  { path: 'login', component: Login },
-  { path: 'register', component: Register },
+
+
   {
-    path: 'admin',
-    component: AdminComponent,
-    canActivate: [AdminGuard],
+    path: '',
+    component: PublicLayoutComponent,
+    children: [
+      { path: 'home', component: Home },
+      { path: 'login', component: Login },
+      { path: 'register', component: Register },
+    ],
   },
+
+
+  {
+    path: '',
+    component: AuthenticatedLayoutComponent,
+    canActivate: [AuthGuard],
+    children: [
+      {
+        path: 'admin',
+        component: AdminComponent,
+        canActivate: [AdminGuard],
+      },
+      {
+        path: 'dashboard',
+        loadComponent: () =>
+          import('./pages/dashboard/dashboard').then(
+            (m) => m.DashboardComponent
+          ),
+      },
+      {
+        path: 'profile',
+        loadComponent: () =>
+          import('./pages/profile/profile').then((m) => m.ProfileComponent),
+      },
+
+    ],
+  },
+
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];

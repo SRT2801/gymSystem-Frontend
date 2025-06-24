a1450cd6c6bba920458e0383551bc5d3fe4df5b3
SRT DEV <sahirruiz4@gmail.com>
2025-06-23 20:47:40 -0500

    FEAT: Add route for admin panel with access guard

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,18 @@ import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
+import { AdminComponent } from './pages/admin/admin';
+import { AdminGuard } from './guards/admin.guard';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'home', pathMatch: 'full' },
   { path: 'home', component: Home },
   { path: 'login', component: Login },
   { path: 'register', component: Register },
+  {
+    path: 'admin',
+    component: AdminComponent,
+    canActivate: [AdminGuard],
+  },
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
